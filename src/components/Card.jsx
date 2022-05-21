7a41fbb11e2e8ae9226370652aadffe26de9159e
Tatiana Tomita <tatiana.tomita88@gmail.com>
2022-05-21 04:48:25 -0300

    commit inicial

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import Card from '@material-ui/core/card'
+import CardContent from '@material-ui/core/CardContent'
+
+export {
+    Card, 
+    CardContent
+}
---
+++ src/containers/Main/components/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { Typography, CardContent } from '../../../components'
+
+export const LabelStyled = styled(Typography)`
+    font-weight: 500;
+    font-size: 1.5rem;
+`
+
+export const ValueStyled = styled(Typography)`
+    font-weight:400;
+    font-size: 2.5rem;
+`
+
+
